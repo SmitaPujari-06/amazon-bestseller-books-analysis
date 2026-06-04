57e9e479ed1fd14431a430244b15630f4dda9abd
SmitaPujari-06 <smitapujari06@gmail.com>
2026-06-04 12:14:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,4 +3,6 @@ import streamlit as st
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("data/Amazon_BestSelling_Books_500.csv")
+    return pd.read_csv(
+        "Amazon bestseller books dataset/Amazon_BestSelling_Books_500.csv"
+    )
